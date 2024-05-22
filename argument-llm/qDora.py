5482f2e4307fb90c614b5e6135dae2d5d2644da3
okoknik <oetkenniklas@gmail.com>
2024-05-22 20:21:30 +0200

    add eval train sh

--- argument-llm/qDora.py
+++ argument-llm/qDora.py
@@ -157,7 +157,7 @@ class TrainingArguments(transformers.TrainingArguments):
     per_device_train_batch_size: int = field(default=8, metadata={"help": 'The training batch size per GPU. Increase for better speed.'})
     #per_device_eval_batch_size: int = field(default=24, metadata={"help":'The evaluation/prediction batch sizer per GPU. Change if out of memory in evaluation/prediction.'})
     gradient_accumulation_steps: int = field(default=16, metadata={"help": 'How many gradients to accumulate before to perform an optimizer step'})
-    max_steps: int = field(default=10111, metadata={"help": 'How many optimizer update steps to take.'})
+    max_steps: int = field(default=110, metadata={"help": 'How many optimizer update steps to take.'})
     weight_decay: float = field(default=0.0, metadata={"help": 'The L2 weight decay rate of AdamW'}) # use lora dropout instead for regularization if needed
     learning_rate: float = field(default=0.0002, metadata={"help": 'The learnign rate'})
     remove_unused_columns: bool = field(default=False, metadata={"help": 'Removed unused columns. Needed to make this codebase work.'})
