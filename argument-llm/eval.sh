3c7213b0e13e39e91421bf0d89801c2804325cb8
okoknik <oetkenniklas@gmail.com>
2024-05-22 20:18:06 +0200

    add eval train sh

---
+++ argument-llm/eval.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -A project_2010365
+#SBATCH -J qDora
+#SBATCH -o qDora.%j.out
+#SBATCH -e qDora.%j.err
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=02:00:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=niklas.oetken@stud.uni-bamberg.de
+
+echo Starting at $(date) 
+set -e 
+# module load python-data/3.10-24.04
+
+# set up wandb api
+export WANDB_API_KEY=
+
+./venv/bin/wandb login 
+./venv/bin/python ./qDora.py --do_train=False --do_eval=True
+
+
+echo Finishing at $(date)
