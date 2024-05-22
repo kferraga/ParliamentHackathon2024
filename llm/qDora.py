10e224cd99df403b83dbfdb78328e1605ba3fc3e
okoknik <oetkenniklas@gmail.com>
2024-05-22 14:57:26 +0200

    add updated data

--- llm/qDora.py
+++ llm/qDora.py
@@ -153,7 +153,7 @@ class TrainingArguments(transformers.TrainingArguments):
     )
     output_dir: str = field(default='./output_cls', metadata={"help": 'The output dir for logs and checkpoints'})
     optim: str = field(default='adamw_torch', metadata={"help": 'The optimizer to be used'})
-    per_device_train_batch_size: int = field(default=128, metadata={"help": 'The training batch size per GPU. Increase for better speed.'})
+    per_device_train_batch_size: int = field(default=64, metadata={"help": 'The training batch size per GPU. Increase for better speed.'})
     #per_device_eval_batch_size: int = field(default=24, metadata={"help":'The evaluation/prediction batch sizer per GPU. Change if out of memory in evaluation/prediction.'})
     gradient_accumulation_steps: int = field(default=16, metadata={"help": 'How many gradients to accumulate before to perform an optimizer step'})
     max_steps: int = field(default=1183, metadata={"help": 'How many optimizer update steps to take.'})
